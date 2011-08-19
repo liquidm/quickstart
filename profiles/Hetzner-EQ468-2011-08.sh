c06d0f9eb76f3a2b8ba9a34361c8dd1953b048ae
Benedikt Böhm <bb@xnull.de>
2011-08-19 17:53:00 +0200

    split profiles

--- profiles/Hetzner-EQ468-2011-08.sh
+++ profiles/Hetzner-EQ468-2011-08.sh
@@ -1,17 +1,10 @@
 # Hetzner EQ4/6/8 profile with Software RAID
 # see http://www.hetzner.de/hosting/produktmatrix/rootserver-produktmatrix-eq
 
-stage_uri http://www.zentoo.org/downloads/amd64/stage4-current.tar.bz2
-tree_type git-snapshot http://www.zentoo.org/downloads/snapshots/portage-current.tar.bz2 11.0
-mirror http://ftp.spline.de/pub/gentoo/distfiles/
+. profiles/base.sh
 
-rootpw icanhazpower
-timezone Europe/Berlin
-
-kernel_sources vserver-sources
 kernel_config_uri https://raw.github.com/hollow/zentoo-quickstart/master/profiles/Hetzner-EQ468-2011-08.kconfig
 
-bootloader grub
 bootloader_install_device /dev/sda1
 bootloader_install_device /dev/sdb1
 
@@ -25,29 +18,9 @@ mdraid md1 --metadata=0.90 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
 mdraid md2 --metadata=0.90 --level=1 --raid-devices=2 /dev/sda2 /dev/sdb2
 
 lvm_volgroup vg /dev/md2
-lvm_logvol vg 10G usr
-lvm_logvol vg 10G var
 
 format /dev/md1 ext3
-format /dev/vg/usr xfs
-format /dev/vg/var xfs
 
 mountfs /dev/md1 ext3 /
-mountfs /dev/vg/usr xfs /usr noatime
-mountfs /dev/vg/var xfs /var noatime
 
-extra_packages mdadm lvm2 xfsprogs
-
-rcadd lvm boot
-rcadd sshd default
-
-net eth0 current
-
-reboot
-
-pre_install() {
-	notify "Setting the system clock"
-	spawn "/etc/init.d/ntp stop"
-	spawn "ntpdate pool.ntp.org"
-	spawn "hwclock --systohc"
-}
+extra_packages mdadm
---
+++ profiles/base.sh
@@ -0,0 +1,33 @@
+stage_uri http://www.zentoo.org/downloads/amd64/stage4-current.tar.bz2
+tree_type git-snapshot http://www.zentoo.org/downloads/snapshots/portage-current.tar.bz2 11.0
+mirror http://ftp.spline.de/pub/gentoo/distfiles/
+
+rootpw icanhazpower
+timezone Europe/Berlin
+
+kernel_sources vserver-sources
+
+lvm_logvol vg 10G usr
+lvm_logvol vg 10G var
+
+format /dev/vg/usr xfs
+format /dev/vg/var xfs
+
+mountfs /dev/vg/usr xfs /usr noatime
+mountfs /dev/vg/var xfs /var noatime
+
+extra_packages lvm2 xfsprogs
+
+rcadd lvm boot
+rcadd sshd default
+
+net eth0 current
+
+reboot
+
+pre_install() {
+	notify "Setting the system clock"
+	spawn "/etc/init.d/ntp stop"
+	spawn "ntpdate pool.ntp.org"
+	spawn "hwclock --systohc"
+}
