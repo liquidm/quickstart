fe831c877f0b33af46d9529c52fb90268e9ca5b9
Benedikt Böhm <bb@xnull.de>
2012-03-01 21:17:15 +0100

    add profile limited to 2TB

---
+++ profiles/2tb-two-disk-md.sh
@@ -0,0 +1,21 @@
+. profiles/base.sh
+
+bootloader_install_device /dev/sda1
+bootloader_install_device /dev/sdb1
+
+part sda 1 fd 1GB
+part sda 2 fd 2046GB
+
+part sdb 1 fd 1GB
+part sdb 2 fd 2046GB
+
+mdraid md1 --metadata=0.90 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
+mdraid md2 --metadata=0.90 --level=1 --raid-devices=2 /dev/sda2 /dev/sdb2
+
+lvm_volgroup vg /dev/md2
+
+format /dev/md1 ext3
+
+mountfs /dev/md1 ext3 /
+
+extra_packages mdadm
