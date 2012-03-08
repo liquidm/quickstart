da37efa4d761242da7a8403b083bb3567bcc0fe5
Benedikt Böhm <bb@xnull.de>
2012-03-08 10:27:18 +0100

    update stage URLs

--- profiles/vagrant-chef-server.sh
+++ profiles/vagrant-chef-server.sh
@@ -1,7 +1,7 @@
 . profiles/virtualbox-single-disk.sh
 . profiles/common/vagrant.sh
 
-stage_uri http://www.zentoo.org/downloads/amd64/zentoo-chef-amd64-current.tar.bz2
+stage_uri http://www.zentoo.org/downloads/amd64/chef-server-current.tar.bz2
 
 post_install() {
 	vagrant_post_install
