dd08d45bbd4de9c3a61ef1aae604d0a65f034678
Benedikt Böhm <bb@xnull.de>
2012-03-09 22:19:38 +0100

    remove self

--- scripts/vbox-additions.sh
+++ scripts/vbox-additions.sh
@@ -13,3 +13,4 @@ rm -f /etc/.pwd.lock /etc/resolv.conf /etc/ssh/ssh_host*
 rm -f /etc/udev/rules.d/70-persistent-*
 rm -rf /var/tmp/* /tmp/* /root/* /var/log/*
 rm -rf /var/cache/edb/dep/*
+rm -rf $(realpath $(dirname $0)/../)
