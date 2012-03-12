23113d7d5fee1477265a1e2fab1923dbaa002051
Benedikt Böhm <bb@xnull.de>
2012-03-12 13:27:32 +0100

    readd some vbox stuff

--- profiles/common/virtualbox.sh
+++ profiles/common/virtualbox.sh
@@ -1,4 +1,28 @@
+# this one is really ugly since it uses 'uname -r' for kernel detection and it
+# cannot be changed easily without breaking checksums, offsets and all kind of
+# nasty voodoo in the installer.
+#
+# as a result, this function should only be used if the source and target setup
+# uses the same kernel release
+install_guest_additions() {
+	cat <<"EOF" > ${chroot_dir}/tmp/vbox.sh
+emerge app-emulation/virtualbox-additions
+
+mount /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/
+/mnt/VBoxLinuxAdditions.run --nox11
+umount /mnt
+
+emerge -C app-emulation/virtualbox-additions
+
+rm -f /tmp/vbox.sh
+EOF
+
+	spawn_chroot "bash /tmp/vbox.sh"
+}
+
 compact_with_cleanup() {
+	spawn_chroot "emerge -C ${kernel_sources}"
+	spawn_chroot "rm -rf /usr/src/linux-* /var/cache/genkernel"
 	spawn_chroot "rm -rf /usr/portage/distfiles/* /usr/portage/packages/*"
 }
 
@@ -6,25 +30,4 @@ compact_with_zero_fill() {
 	for part in / /usr /var; do
 		spawn_chroot "cat /dev/zero > ${part}/zero.fill; sync; rm -f ${part}/zero.fill; sync"
 	done
-}
-
-post_install() {
-	# setup hostname
-	echo "127.0.0.1 vagrant-zentoo.vagrantup.com vagrant-zentoo localhost" > ${chroot_dir}/etc/hosts
-	echo "hostname=\"vagrant-zentoo\"" > ${chroot_dir}/etc/conf.d/hostname
-
-	# setup user
-	spawn_chroot "useradd -m -d /home/vagrant -g users -G wheel,portage,cron vagrant"
-	spawn_chroot "echo vagrant:vagrant | chpasswd"
-	spawn_chroot "curl -s -k -L https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys"
-
-	# sudo configuration
-	cat <<EOF > ${chroot_dir}/etc/sudoers
-Defaults env_keep="EDITOR SSH_AUTH_SOCK"
-root    ALL = (ALL) ALL
-%wheel  ALL = (ALL) NOPASSWD: ALL
-EOF
-
-	# do not return with failure
-	true
 }
--- profiles/virtualbox-single-disk.sh
+++ profiles/virtualbox-single-disk.sh
@@ -17,6 +17,7 @@ net eth0 dhcp
 shutdown
 
 post_install() {
+	install_guest_additions
 	compact_with_cleanup
 	compact_with_zero_fill
 
