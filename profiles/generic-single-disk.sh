5639caa789fec46fc93fdda259c3680e8e71739e
Benedikt Böhm <bb@xnull.de>
2012-03-29 11:43:21 +0200

    add single-disk profile (for hardware raid etc)

---
+++ profiles/generic-single-disk.sh
@@ -0,0 +1,13 @@
+. profiles/common/base.sh
+. profiles/common/net-current-reboot.sh
+
+bootloader_install_device /dev/sda1
+
+part sda 1 fd 1GB
+part sda 2 fd +
+
+lvm_volgroup vg /dev/sda2
+
+format /dev/sda1 ext3
+
+mountfs /dev/sda1 ext3 /
