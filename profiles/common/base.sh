86761516d8521229ef18f10cde268d8ebf339969
Benedikt Böhm <bb@xnull.de>
2012-07-31 19:25:56 +0200

    only use extra volumes for generic profiles

--- profiles/common/base.sh
+++ profiles/common/base.sh
@@ -8,23 +8,14 @@ timezone Europe/Berlin
 kernel_sources zentoo-sources
 kernel_config_uri https://raw.github.com/zentoo/quickstart/master/profiles/3.2.14-zentoo-generic.kconfig
 
-lvm_logvol vg 10G home
-lvm_logvol vg 10G opt
 lvm_logvol vg 10G usr
-lvm_logvol vg 64G var
-lvm_logvol vg 32G log
+lvm_logvol vg 10G var
 
-format /dev/vg/home xfs
-format /dev/vg/opt xfs
 format /dev/vg/usr xfs
 format /dev/vg/var xfs
-format /dev/vg/log xfs
 
-mountfs /dev/vg/home xfs /home noatime
-mountfs /dev/vg/opt xfs /opt noatime
 mountfs /dev/vg/usr xfs /usr noatime
 mountfs /dev/vg/var xfs /var noatime
-mountfs /dev/vg/log xfs /var/log noatime
 
 extra_packages lvm2 xfsprogs
 
