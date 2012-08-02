9c308be5cd3580b2c85a7319fadd563fd2a7e3f8
Benedikt Böhm <bb@xnull.de>
2012-08-02 11:07:14 +0200

    use newer kernel config

--- profiles/common/base.sh
+++ profiles/common/base.sh
@@ -6,7 +6,7 @@ rootpw tux
 timezone Europe/Berlin
 
 kernel_sources zentoo-sources
-kernel_config_uri https://raw.github.com/zentoo/quickstart/master/profiles/3.2.14-zentoo-generic.kconfig
+kernel_config_uri https://raw.github.com/zentoo/quickstart/master/profiles/3.2.15-zentoo-generic.kconfig
 
 lvm_logvol vg 10G usr
 lvm_logvol vg 10G var
