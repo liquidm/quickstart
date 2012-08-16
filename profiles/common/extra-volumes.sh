c757000e88a8193ba9bb0c9edba3aa32da09bfd3
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-08-16 14:40:38 +0200

    deduplicate format directive in common profiles

--- profiles/common/extra-volumes.sh
+++ profiles/common/extra-volumes.sh
@@ -5,10 +5,8 @@ lvm_logvol vg 32G log
 
 format /dev/vg/home xfs
 format /dev/vg/opt xfs
-format /dev/vg/var xfs
 format /dev/vg/log xfs
 
 mountfs /dev/vg/home xfs /home noatime
 mountfs /dev/vg/opt xfs /opt noatime
-mountfs /dev/vg/var xfs /var noatime
 mountfs /dev/vg/log xfs /var/log noatime
