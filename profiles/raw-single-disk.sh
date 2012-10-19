2c282ffce947b428f26c8b74a3ba1619ec84411f
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-10-19 11:34:48 +0200

    add special profile for raw cluster

---
+++ profiles/raw-single-disk.sh
@@ -0,0 +1,14 @@
+. profiles/common/base.sh
+. profiles/common/extra-volumes.sh
+. profiles/common/net-current-reboot.sh
+
+bootloader_install_device /dev/sdc
+
+part sda 1 fd00 1G
+part sda 2 fd00
+
+lvm_volgroup vg /dev/sda2
+
+format /dev/sda1 ext3
+
+mountfs /dev/sda1 ext3 /
