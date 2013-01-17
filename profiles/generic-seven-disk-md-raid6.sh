5a59ff1f844a4819053de8d4ee783aebee38009b
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-01-17 11:48:04 +0100

    switch to raid6 and add 7 disk profile

--- profiles/generic-seven-disk-md-raid6.sh
+++ profiles/generic-seven-disk-md-raid6.sh
@@ -7,6 +7,9 @@ bootloader_install_device /dev/sda
 bootloader_install_device /dev/sdb
 bootloader_install_device /dev/sdc
 bootloader_install_device /dev/sdd
+bootloader_install_device /dev/sde
+bootloader_install_device /dev/sdf
+bootloader_install_device /dev/sdg
 
 part sda 1 fd00 1G
 part sda 2 fd00
@@ -20,8 +23,17 @@ part sdc 2 fd00
 part sdd 1 fd00 1G
 part sdd 2 fd00
 
-mdraid md1 --level=1 --raid-devices=4 /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1
-mdraid md2 --level=5 --raid-devices=4 /dev/sda2 /dev/sdb2 /dev/sdc2 /dev/sdd2
+part sde 1 fd00 1G
+part sde 2 fd00
+
+part sdf 1 fd00 1G
+part sdf 2 fd00
+
+part sdg 1 fd00 1G
+part sdg 2 fd00
+
+mdraid md1 --level=1 --raid-devices=4 /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1
+mdraid md2 --level=6 --raid-devices=4 /dev/sda2 /dev/sdb2 /dev/sdc2 /dev/sdd2 /dev/sde2 /dev/sdf2 /dev/sdg2
 
 lvm_volgroup vg /dev/md2
 
