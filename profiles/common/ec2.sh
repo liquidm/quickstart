f446732f2e0c9eda98067234b75f47262d318bd4
Benedikt Böhm <bb@xnull.de>
2013-02-04 11:04:59 +0100

    hardcode /dev/xvda1 for EC2 pv-grub boot

--- profiles/common/ec2.sh
+++ profiles/common/ec2.sh
@@ -5,9 +5,6 @@ need_mbr
 net eth0 dhcp
 
 setup_pv_grub() {
-    local root_device=$1
-    local root_uuid=$(blkid -s UUID -o value ${root_device})
-
     mkdir -p /mnt/gentoo/boot/grub
     cat <<EOG > /mnt/gentoo/boot/grub/menu.lst
 default 0
@@ -15,7 +12,7 @@ timeout 10
 
 title Gentoo Linux
 root (hd0,0)
-kernel /boot/kernel root=UUID=${root_uuid} ro domdadm dolvm
+kernel /boot/kernel root=/dev/xvda1 ro domdadm dolvm
 initrd /boot/initramfs
 EOG
 }
