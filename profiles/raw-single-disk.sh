5e7b89322c840bcd7d9af69b9d0ba96e5a7b2d9e
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-05-03 09:03:09 +0200

    update profile for raw hosts

--- profiles/raw-single-disk.sh
+++ profiles/raw-single-disk.sh
@@ -5,8 +5,17 @@
 part sdc 1 fd00 1G
 part sdc 2 fd00
 
+format /dev/sdc1 ext3
+mountfs /dev/sdc1 ext3 /
+
 lvm_volgroup vg /dev/sdc2
 
-format /dev/sdc1 ext3
+format /dev/sda xfs
+mountfs /dev/sda xfs /mnt/hadoop/a noatime
 
-mountfs /dev/sdc1 ext3 /
+format /dev/sdb xfs
+mountfs /dev/sdb xfs /var/tmp/hadoop noatime
+
+lvm_logvol vg 500G javatmp
+format /dev/vg/javatmp xfs
+mountfs /dev/vg/javatmp xfs /var/tmp/java noatime
