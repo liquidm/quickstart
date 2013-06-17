2c4cc0e19f759d9935a37d8f4411341a74639a43
Benedikt Böhm <bb@xnull.de>
2013-06-17 08:24:48 +0200

    update after merge

--- profiles/generic-seven-disk-md-raid6.sh
+++ profiles/generic-seven-disk-md-raid6.sh
@@ -3,14 +3,6 @@
 . profiles/common/md.sh
 . profiles/common/net-current-reboot.sh
 
-bootloader_install_device /dev/sda
-bootloader_install_device /dev/sdb
-bootloader_install_device /dev/sdc
-bootloader_install_device /dev/sdd
-bootloader_install_device /dev/sde
-bootloader_install_device /dev/sdf
-bootloader_install_device /dev/sdg
-
 part sda 1 fd00 1G
 part sda 2 fd00
 
@@ -32,8 +24,8 @@ part sdf 2 fd00
 part sdg 1 fd00 1G
 part sdg 2 fd00
 
-mdraid md1 --level=1 --raid-devices=4 /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1
-mdraid md2 --level=6 --raid-devices=4 /dev/sda2 /dev/sdb2 /dev/sdc2 /dev/sdd2 /dev/sde2 /dev/sdf2 /dev/sdg2
+mdraid md1 --level=1 --raid-devices=7 /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1
+mdraid md2 --level=6 --raid-devices=7 /dev/sda2 /dev/sdb2 /dev/sdc2 /dev/sdd2 /dev/sde2 /dev/sdf2 /dev/sdg2
 
 lvm_volgroup vg /dev/md2
 
