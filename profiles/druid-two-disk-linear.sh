210b54f06fd741223ac08143229d595e5f855d8a
Benedikt Böhm <bb@xnull.de>
2013-07-23 11:20:04 +0200

    add druid linear profile

---
+++ profiles/druid-two-disk-linear.sh
@@ -0,0 +1,28 @@
+. profiles/common/base.sh
+. profiles/common/md.sh
+. profiles/common/net-current-reboot.sh
+
+part sda 1 fd00 1G
+part sda 2 fd00
+
+part sdb 1 fd00 1G
+part sdb 2 fd00
+
+mdraid md1 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
+mdraid md2 --level=linear --raid-devices=2 /dev/sda2 /dev/sdb2
+
+lvm_volgroup vg /dev/md2
+
+format /dev/md1 ext3
+
+mountfs /dev/md1 ext3 /
+
+lvm_logvol vg 10G opt
+lvm_logvol vg 384G var
+lvm_logvol vg 32G log
+
+format /dev/vg/opt xfs
+format /dev/vg/log xfs
+
+mountfs /dev/vg/opt xfs /opt noatime
+mountfs /dev/vg/log xfs /var/log noatime
