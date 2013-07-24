efb9a8a8c26bed4e24175ebb5315aa4cbafad0b3
Benedikt Böhm <bb@xnull.de>
2013-07-24 13:13:08 +0200

    fix typo

--- profiles/nohome-two-disk-raid0.sh
+++ profiles/nohome-two-disk-raid0.sh
@@ -9,7 +9,7 @@ part sdb 1 fd00 1G
 part sdb 2 fd00
 
 mdraid md1 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
-mdraid md2 --level=linear --raid-devices=2 /dev/sda2 /dev/sdb2
+mdraid md2 --level=0 --raid-devices=2 /dev/sda2 /dev/sdb2
 
 lvm_volgroup vg /dev/md2
 
