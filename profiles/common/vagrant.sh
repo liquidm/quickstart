9ab8f465968f29e5dbe132c5ef9987075cec912d
Benedikt Böhm <bb@xnull.de>
2013-09-30 12:02:42 +0200

    move guest addition install to vagrant bootstrap

--- profiles/common/vagrant.sh
+++ profiles/common/vagrant.sh
@@ -14,7 +14,6 @@ EOF
 
 post_install() {
 	vagrant_post_install
-	install_guest_additions
 	compact_with_cleanup
 	compact_with_zero_fill
 
--- profiles/common/virtualbox.sh
+++ profiles/common/virtualbox.sh
@@ -1,33 +1,3 @@
-# this one is really ugly since it uses 'uname -r' for kernel detection and it
-# cannot be changed easily without breaking checksums, offsets and all kind of
-# nasty voodoo in the installer.
-#
-# as a result, this function should only be used if the source and target setup
-# uses the same kernel release
-install_guest_additions() {
-	cat <<"EOF" > ${chroot_dir}/tmp/vbox.sh
-set -e
-
-emerge sys-kernel/gentoo-sources
-pushd /usr/src/linux
-cp /boot/config-* .config
-make modules_prepare
-popd
-
-emerge app-emulation/virtualbox-additions
-mount /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/
-/mnt/VBoxLinuxAdditions.run --nox11
-umount /mnt
-emerge -C app-emulation/virtualbox-additions
-
-emerge -C sys-kernel/gentoo-sources
-
-rm -f /tmp/vbox.sh
-EOF
-
-	spawn_chroot "bash /tmp/vbox.sh"
-}
-
 compact_with_cleanup() {
 	spawn_chroot "rm -rf /usr/src/linux-* /var/cache/genkernel"
 	spawn_chroot "rm -rf /usr/portage/distfiles/* /usr/portage/packages/*"
@@ -40,7 +10,6 @@ compact_with_zero_fill() {
 }
 
 post_install() {
-	install_guest_additions
 	compact_with_cleanup
 	compact_with_zero_fill
 
