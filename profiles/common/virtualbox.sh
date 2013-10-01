7a291ec29681df582ec5341eb7f6c2f4fede662a
Benedikt Böhm <bb@xnull.de>
2013-10-01 13:27:12 +0200

    remove very slow compact

--- profiles/common/virtualbox.sh
+++ profiles/common/virtualbox.sh
@@ -31,20 +31,13 @@ EOF
 }
 
 compact_with_cleanup() {
-	spawn_chroot "rm -rf /usr/src/linux-* /var/cache/genkernel"
+	spawn_chroot "rm -rf /var/cache/genkernel"
 	spawn_chroot "rm -rf /usr/portage/distfiles/* /usr/portage/packages/*"
-}
-
-compact_with_zero_fill() {
-	for part in / /usr /var; do
-		spawn_chroot "cat /dev/zero > ${part}/zero.fill; sync; rm -f ${part}/zero.fill; sync"
-	done
 }
 
 post_install() {
 	install_guest_additions
 	compact_with_cleanup
-	compact_with_zero_fill
 
 	# do not return with failure
 	true
