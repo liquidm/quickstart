f4585870ec132c9ed6afd89b9af08fa6eb2cfc56
Benedikt Böhm <bb@xnull.de>
2014-06-11 08:22:04 +0200

    add minimal zpool profile

---
+++ profiles/zpool-two-disk-md.sh
@@ -0,0 +1,13 @@
+. profiles/common/base.sh
+
+part sda 1 fd00 32G
+part sda 2 a504
+
+part sdb 1 fd00 32G
+part sdb 2 a504
+
+mdraid md1 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
+
+format /dev/md1 ext4
+
+mountfs /dev/md1 ext4 /
