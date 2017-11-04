254d3d1dc885d2261c625251ede673291f280209
Hagen Rother <hagen.rother@liquidm.com>
2017-11-04 22:41:36 +0100

    don't grub legacy by default, too many efis by now

--- modules/install_steps.sh
+++ modules/install_steps.sh
@@ -169,14 +169,6 @@ install_kernel() {
     # stock kernel
     spawn_chroot "DEBIAN_FRONTEND=noninteractive apt-get -y install ${kernel_image}" || die "could not install kernel"
 
-    # grub it to all disks
-    # (note: grub will successfully install to nvme0n1,
-    #        but but the bios will not necessarily boot it from there)
-    for x in /dev/sd[a-z] /dev/nvme[0-9]n1; do
-      if [ -e $x ]; then
-        spawn_chroot "/usr/sbin/grub-install $x"
-      fi
-    done
   fi
 }
 
